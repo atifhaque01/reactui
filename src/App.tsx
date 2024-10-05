9786579563b037258842a4cff6cd90e0aa3986bb
Atif Haque <atifhaque01@vt.edu>
2024-10-05 19:57:47 -0400

    Added graph

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,28 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import React, { useCallback, useEffect } from 'react';
-import ReactFlow, { useNodesState, useEdgesState, addEdge, Connection, Edge, Node } from 'reactflow';
-import { getGraphNodes } from './Hooks/getGraphNodes';
-import { GraphNode } from './Types/GraphNode.type';
-import { getGraphEdges } from './Hooks/getGraphEdges';
-import { GraphEdge } from './Types/GraphEdge.type';
-
 import 'reactflow/dist/style.css';
-import { get } from 'http';
+import { Tree } from './Tree';
+import rawFamily from "./tests/family1.json";
+import { RawFamilyMember, RawFamilyRelation } from './utils';
 
-const initialNodes = [
-  { id: '1', position: { x: 50, y: 0 }, data: { label: '1' } },
-  { id: '2', position: { x: 50, y: 100 }, data: { label: '2' } },
-];
-const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
+const familyMembers: RawFamilyMember[] = rawFamily.familyMembers.map((member: any) => ({
+  id: member.id,
+  data: member.data
+}));
+
+const familyRelations: RawFamilyRelation[] = rawFamily.familyRelations.map((relation: any) => ({
+  relationType: relation.relationType,
+  prettyType: relation.prettyType,
+  fromId: relation.fromId,
+  toId: relation.toId,
+  isInnerFamily: relation.isInnerFamily
+}));
 
 export default function App() {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
-  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
-
-  useEffect(() => {
-    getGraphNodes().then((data: GraphNode[]) => {
-      let formattedNodes: Node[] = [];
-      data.forEach((node: GraphNode) => {
-        formattedNodes.push({
-          id: node._id || '',
-          position: { x: node.x || 0, y: node.y || 0 },
-          data: { label: node.member.name },
-        });
-      });
-      setNodes(formattedNodes);
-    });
-
-    getGraphEdges().then((data: GraphEdge[]) => {
-      let formattedEdges: Edge[] = [];
-      data.forEach((edge: GraphEdge) => {
-        formattedEdges.push({
-          id: edge._id || '',
-          source: edge.source || '',
-          target: edge.target || '',
-        });
-      });
-      setEdges(formattedEdges);
-    });
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  const onConnect = useCallback(
-    (params: Edge | Connection) => setEdges((eds) => addEdge(params, eds)),
-    [setEdges],
-  );
-
   return (
-    <div style={{ width: '100vw', height: '100vh' }}>
-      <ReactFlow
-        nodes={nodes}
-        edges={edges}
-        onNodesChange={onNodesChange}
-        onEdgesChange={onEdgesChange}
-        onConnect={onConnect}
+    <div style={{ height: '100vh', width: '100vw' }}>
+      <Tree 
+        members={familyMembers} 
+        relations={familyRelations} 
+        rootId={"0"}
       />
     </div>
   );
---
+++ src/Tree.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./index.css";
+import { ReactFlowProvider } from "reactflow";
+import { RawFamilyMember, buildFamilyAndRelations, RawFamilyRelation } from "./utils";
+// import rawFamily from "../tests/family1.json";
+import { FamilyTree } from "./FamilyTree";
+import "reactflow/dist/style.css";
+
+interface TreeProps {
+    members: RawFamilyMember[];
+    relations: RawFamilyRelation[];
+    rootId?: string;
+}
+
+export const Tree: React.FC<TreeProps> = ({ members, relations, rootId }) => {
+    const [familyMembersRecord, familyRelationsRecord] = buildFamilyAndRelations(members as RawFamilyMember[],
+        relations as RawFamilyRelation[]);
+    const rootMember = rootId ? familyMembersRecord[rootId] : Object.values(familyMembersRecord)[0];
+    return <React.StrictMode>
+        <ReactFlowProvider>
+            <ReactFlowProvider>
+                <div style={{ height: "100vh", width: "100vw" }}>
+                    <FamilyTree
+                        familyMembers={familyMembersRecord}
+                        familyRelations={familyRelationsRecord}
+                        rootMember={rootMember}
+                    />
+                </div>
+            </ReactFlowProvider>
+        </ReactFlowProvider>
+    </React.StrictMode>;
+};
