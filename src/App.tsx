7d4d773606a29f4a88ec29d7f912df3aa9d53653
Atif Haque <atifhaque01@vt.edu>
2024-10-05 20:22:04 -0400

    Added api calls

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,48 @@
 import 'reactflow/dist/style.css';
 import { Tree } from './Tree';
-import rawFamily from "./tests/family1.json";
+import { useEffect, useState } from 'react';
 import { RawFamilyMember, RawFamilyRelation } from './utils';
 
-const familyMembers: RawFamilyMember[] = rawFamily.familyMembers.map((member: any) => ({
-  id: member.id,
-  data: member.data
-}));
-
-const familyRelations: RawFamilyRelation[] = rawFamily.familyRelations.map((relation: any) => ({
-  relationType: relation.relationType,
-  prettyType: relation.prettyType,
-  fromId: relation.fromId,
-  toId: relation.toId,
-  isInnerFamily: relation.isInnerFamily
-}));
-
 export default function App() {
+  const [familyMembers, setFamilyMembers] = useState<RawFamilyMember[]>([]);
+  const [familyRelations, setFamilyRelations] = useState<RawFamilyRelation[]>([]);
+
+  async function fetchFamilyData() {
+    try {
+      const getAllMembers = await fetch('localhost:3012/family/getAllMembers');
+      const responseMembers = await getAllMembers.json();
+
+      const members: RawFamilyMember[] = responseMembers.map((member: any) => ({
+        id: member.id,
+        data: member.data
+      }));
+
+      const getAllRelationships = await fetch('localhost:3012/family/getAllRelationships');
+      const responseRelationships = await getAllRelationships.json();
+
+      const relations: RawFamilyRelation[] = responseRelationships.map((relation: any) => ({
+        relationType: relation.relationType,
+        prettyType: relation.prettyType,
+        fromId: relation.fromId,
+        toId: relation.toId,
+        isInnerFamily: relation.isInnerFamily
+      }));
+
+      setFamilyMembers(members);
+      setFamilyRelations(relations);
+    } catch (error) {
+      console.error('Error fetching family data:', error);
+    }
+  }
+
+  useEffect(() => {
+    fetchFamilyData();
+  }, []);
   return (
     <div style={{ height: '100vh', width: '100vw' }}>
-      <Tree 
-        members={familyMembers} 
-        relations={familyRelations} 
+      <Tree
+        members={familyMembers}
+        relations={familyRelations}
         rootId={"0"}
       />
     </div>
